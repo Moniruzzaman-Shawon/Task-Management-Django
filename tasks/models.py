676bb9c361d72c4c1ef3509cc74b2008f413107b
Moniruzzaman Shawon <m.zaman.djp@gmail.com>
2025-06-24 12:44:50 +0600

    implemented get & post

--- tasks/models.py
+++ tasks/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 
+class Project(models.Model):
+    name = models.CharField( max_length=50)
+    start_date = models.DateField()
+
 # Create your models here.
 class Task(models.Model):
+    project = models.ForeignKey ( Project, on_delete=models.CASCADE, default=1)
     title = models.CharField(max_length=250)
     description = models.TextField()
     due_date = models.DateField()
     is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
 
 # One to One
 # Many to One
@@ -25,3 +31,5 @@ class TaskDetail(models.Model):
     task = models.OneToOneField(Task, on_delete=models.CASCADE)
     assigned_to = models.CharField(max_length=100)
     priority = models.CharField(max_length=1, choices=PRIORITY_OPTIONS, default=LOW)
+
+
--- tasks/views.py
+++ tasks/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 # Create your views here.
 
 def manager_dashboard(request):
-    return render (request, "dashboard/manager-dashboard.html")
+    return render(request, "dashboard/manager-dashboard.html")
 
 def user_dashboard(request):
-    return render (request, "dashboard/user-dashboard.html")
+    return render(request, "dashboard/user-dashboard.html")
 
 def test(request):
     context = {
-         'names' : ['Alice', 'Bob', 'John'],
-         'age' : [23,35,45],
+        'names': ['Alice', 'Bob', 'John'],
+        'age': [23, 35, 45],
     }
-    return render (request, 'test.html', context)
+    return render(request, 'test.html', context)
+
+def create_task(request):
+    return render(request, 'task_form.html')  
