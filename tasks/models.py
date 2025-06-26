692855b9c78e320e56be9dc38474f865c376a588
Moniruzzaman Shawon <m.zaman.djp@gmail.com>
2025-06-27 00:14:49 +0600

    retriving data

--- tasks/models.py
+++ tasks/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Employee(models.Model):
     name = models.CharField(max_length=250)
-    emai = models.EmailField(unique=True)
+    email = models.EmailField(unique=True)
 
     def __str__(self):
         return self.name
@@ -45,16 +45,24 @@ class Task(models.Model):
 # Many to Many
 
 class TaskDetail(models.Model):
-    HIGH ='H'
+    HIGH = 'H'
     MEDIUM = 'M'
-    LOW ='L'
+    LOW = 'L'
     PRIORITY_OPTIONS = (
         (HIGH, 'High'),
         (MEDIUM, 'Medium'),
         (LOW, 'Low')
     )
-    task = models.OneToOneField(Task, on_delete=models.CASCADE)
+    task = models.OneToOneField(
+        Task,
+        on_delete=models.CASCADE,
+        related_name='details',
+    )
     assigned_to = models.CharField(max_length=100)
-    priority = models.CharField(max_length=1, choices=PRIORITY_OPTIONS, default=LOW)
+    priority = models.CharField(
+        max_length=1, choices=PRIORITY_OPTIONS, default=LOW)
+    notes = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return f"Details form Task {self.task.title}"
 
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from tasks.forms import TaskForm
-from tasks.models import Employee
+from tasks.models import Employee, Task, TaskDetail, Project
 
 # Create your views here.
 
@@ -23,3 +23,13 @@ def create_task(request):
     form = TaskForm(employees=employees)
     context = {"form": form}
     return render(request, 'task_form.html', context)  
+
+
+def view_task(request):
+    # retrive all data from task model
+    task = Task.objects.all()
+
+    #retrive a specific task
+    task_3 = Task.objects.get(id = 3)
+
+    return render(request, 'show_task.html', {"tasks" : task, "task3" : task_3})
