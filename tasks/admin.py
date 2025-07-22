c4722c25a588a36c2d708b1fc6876bb07d6e62c2
Moniruzzaman Shawon <m.zaman.djp@gmail.com>
2025-07-23 04:02:49 +0600

    user profile added

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from tasks.models import Task, TaskDetail, Employee, Project
+from users.models import UserProfile  # Replace with actual model location
+
 
 # Register your models here.
 admin.site.register(Task)
 admin.site.register(TaskDetail)
 admin.site.register(Employee)
 admin.site.register(Project)
+
+
+admin.site.register(UserProfile)
